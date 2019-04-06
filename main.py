672e2de5d1ab23e75c908ed4c49210dadd9546eb
Bruno Salerno <br.salerno@gmail.com>
2019-04-06 17:41:13 +0200

    Fetch multiple trips at the same time

--- main.py
+++ main.py
@@ -10,31 +10,29 @@ api_key      = os.environ['API_KEY']
 service      = os.environ['SERVICE']
 origin       = os.environ['ORIGIN']
 destination  = os.environ['DESTINATION']
-current_journey_id  = None
-
-# Every one minute I fetch the data for the current_journey_id
-# If the last stop has been reached, I fetch a new journey_id
+current_journey_ids = []
 
 while True:
     print('-> ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-    if current_journey_id is None:
-        trip = TripPlan(service, origin, destination, api_key)
-        current_journey_id = trip.next_journey_id()
-        if current_journey_id is None:
-            print('Error fetching journey. Skipping')
-        else:
-            print('=> New journey: ' + current_journey_id)
+    trip = TripPlan(service, origin, destination, api_key)
+    journey_id = trip.next_journey_id()
+    if journey_id is None:
+        print('Error fetching journey. Skipping')
     else:
-        print ('(current journey id: ' + current_journey_id +')')
+        if journey_id not in current_journey_ids:
+            current_journey_ids.append(journey_id)
+            print('=> Added journey: ' + journey_id)
 
-    if current_journey_id is not None:
-        journey = Journey(current_journey_id, api_key)
+    for journey_id in current_journey_ids:
+        print ('(journey id: ' + journey_id +')')
+
+        journey = Journey(journey_id, api_key)
         if journey.has_stops_info():
             journey.print()
             if journey.finished():
+                current_journey_ids.remove(journey_id)
                 journey.to_csv()
-                current_journey_id = None
                 print('=> Destination reached')
         else:
             print('Error with the journey data. Skipping')
